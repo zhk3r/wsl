0fe66cfda1d1c30a0b5ea445f8d8c9258920e054
zhk3r <37957791+zhk3r@users.noreply.github.com>
2023-12-22 16:19:34 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -31,8 +31,16 @@ sed -i '/plugins=(git)/a plugins=(zsh-syntax-highlighting)\nplugins=(zsh-autosug
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="simple-path"/' ~/.zshrc
 
 # Apply changes to the current shell session
-source .zshrc
+source ~/.zshrc
 
 # Restart shell to apply changes
 exec zsh
 exec bash -l
+
+# Prompt to restart shell
+read -p "Restart shell now? (y/n): " -n 1 -r
+echo
+if [[ \$REPLY =~ ^[Yy]$ ]]
+then
+    exec zsh
+fi
